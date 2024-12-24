151fb60e6a2043b26d8b91a283f06c7c97e9569d
julian-rinaudo <julianrinaudo18@gmail.com>
2024-12-24 00:51:36 -0300

    Add: layout and pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { SearchBar } from "@/components/SearchBar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SWRProvider } from "@/app/swr-provider";
