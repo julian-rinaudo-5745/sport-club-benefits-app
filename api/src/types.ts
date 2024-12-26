04908035c0dbcfd214129bcf01e4b9774f514a84
julian-rinaudo <julianrinaudo18@gmail.com>
2024-12-26 15:39:08 -0300

    Add: workspaces with concurrently

--- api/src/types.ts
+++
@@ -1,7 +0,0 @@
-const TYPES = {
-  BenefitRepositoryInterface: Symbol.for("BenefitRepositoryInterface"),
-  BenefitsService: Symbol.for("BenefitsService"),
-  BenefitsController: Symbol.for("BenefitsController"),
-};
-
-export { TYPES };
--- app/src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export const SPORT_CLUB_LOGO =
-  "https://beneficios.sportclub.com.ar/_next/static/media/logo-sportclub-black.126e7e0d.webp";
